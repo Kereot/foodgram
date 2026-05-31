23092f53856b6c7ac9330a9977d47f09ac27f19f
Dmitrii Balakin <eepdmitriy@gmail.com>
2026-05-31 11:51:59 +0400

    4th week. Corrections after 2nd review.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserSerializer
-from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from api.fields import NoBlankBase64ImageField
@@ -114,7 +113,7 @@ class RecipeIngredientWriteSerializer(serializers.ModelSerializer):
 
 class RecipeBasicReadSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(source='pk', read_only=True)
-    image = Base64ImageField(required=True, allow_null=False)
+    image = NoBlankBase64ImageField(required=True, allow_null=False)
 
     class Meta:
         fields = (
@@ -170,7 +169,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         queryset=Tag.objects.all(),
         many=True
     )
-    image = Base64ImageField(required=True, allow_null=False)
+    image = NoBlankBase64ImageField(required=True, allow_null=False)
     cooking_time = serializers.IntegerField(
         min_value=MIN_POSITIVE_INTEGER_FIELD,
         max_value=MAX_SMALL_POSITIVE_INTEGER_FIELD,
@@ -188,10 +187,14 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         model = Recipe
         read_only_fields = ('author',)
 
+    # Я сделал поле image везде обязательным. Его проверка в методе ниже
+    # ломается при редактировании рецепта и базовом поле Base64ImageField.
+    # Можно сделать в методе def validate_image - но зачем,
+    # кастомный класс поля всё равно используется в аватаре, и в этом классе
+    # такая же проверка. Так что я вернулся к его использованию здесь.
     def validate(self, attrs):
         validate_required_field('tags', attrs)
         validate_required_field('recipe_ingredients', attrs)
-        validate_required_field('image', attrs)
         validate_unique_field('tags', attrs)
         validate_unique_field('recipe_ingredients', attrs, True)
         return attrs
